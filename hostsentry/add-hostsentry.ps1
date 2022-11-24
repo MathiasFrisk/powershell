7f0a0bf1c8e4ede667481c2c00c33ff847a726d7
Mathias <77238760+MathiasShit@users.noreply.github.com>
2022-11-24 10:24:14 +0100

    idk

--- hostsentry/add-hostsentry.ps1
+++ hostsentry/add-hostsentry.ps1
@@ -1,4 +1,4 @@
-$file = 'W:\Repositories\powershell\hosts'
+$file = 'C:\Users\OleMathiasMortensenF\OneDrive - Procano AS\Dokumenter\Github\powershell\hostsentry\hosts'
 
 $hostfile = Get-Content $file
 $hostfile += "8.8.8.8   Google.com"
